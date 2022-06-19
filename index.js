b86947cbcb933bc46bc60eb448e42c819068e5ee
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-19 12:25:23 +0300

    commit tree 3f56a79a160b19c9a35e20db4ba9b398363a037b

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordString = word.toLowerCase();
+  let splitWord = wordString.split("");
+  let reverseWord = splitWord.reverse();
+  let joinWord = reverseWord.join("")
+
+
+ 
+if (joinWord === wordString) {
+  return true;
 }
+
+else if (joinWord !== wordString) {
+  return false;
+}
+
+}
+
+console.log(isPalindrome("abba"));
+
 
 /* 
   Add your pseudocode here
+  Take the word as an input and change it to lowercase. 
+  Then split it.
+  Then revervse it.
+  Join the elements.
+  compare if the the joined-reverse word is equal to the lower-cased word.
+  If equal, return true as an output.
+  If not, return false as an output.
+NB: all the above should happen inside isPalindrome function.
+Finally, test the result.
+
 */
 
 /*
